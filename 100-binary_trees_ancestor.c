db25a34af4ae0572afa75a9c1e0a8cd7fea05bae
tenderheartviv <vivian.emenike@yahoo.com>
2023-09-01 21:31:37 -0700

    100-binary_trees_ancestor

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - this finds the lowest common ancestor of two nodes
+ * @first: pointing to the first node
+ * @second: pointing to the second node
+ * Return: pointing to the lowest common ancestor node of the two given nodes
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	binary_tree_t *tmp;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+	tmp = (binary_tree_t *)second;
+	while (first)
+	{
+		while (second)
+		{
+			if (first == second)
+				return ((binary_tree_t *)first);
+			second = second->parent;
+		}
+		second = tmp;
+		first = first->parent;
+	}
+	return (NULL);
+}
