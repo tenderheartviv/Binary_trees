a099966c1c7b4b7fa259afd44db73775a56719a7
tenderheartviv <vivian.emenike@yahoo.com>
2023-09-01 20:17:05 -0700

    3-binary_tree_delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_delete - this recursively traverses `binary_tree_t` binary tree
+ * (base case when reaching leaves' NULL `left` and `right`) to delete
+ * all nodes
+ * @tree: pointing to root node of tree or subtree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
+}
